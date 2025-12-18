cff5700b81a798231855f3b8e54d33f03732718e
icad08 <irsyadrm08@gmail.com>
2025-12-18 09:37:27 +0700

    add private val retrofitService

--- app/src/main/java/com/example/myapi/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myapi/repositori/ContainerApp.kt
@@ -2,31 +2,39 @@ package com.example.myapi.repositori
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 
-interface ContainerApp{
-    val repositoryDataSiswa : repositoryDataSiswa
+interface ContainerApp {
+    val repositoryDataSiswa: RepositoryDataSiswa
 }
 
-class DefaultContainerApp : ContainerApp{
-    private val beseurl = " "
-    val logging HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+class DefaultContainerApp : ContainerApp {
+    private val beseurl = "http://10.0.2.2:3000/"
 
-    val klien = okHttpClient.Builder()
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val klien = OkHttpClient.Builder()
         .addInterceptor(logging)
         .build()
 
     private val retrofit = Retrofit.Builder()
         .baseUrl(beseurl)
         .addConverterFactory(
-            Json{
+            Json {
                 ignoreUnknownKeys = true
                 prettyPrint = true
                 isLenient = true
             }.asConverterFactory("application/json".toMediaType())
         )
         .client(klien)
-        .build()")
-        )
+        .build()
+
+    private val retrofitService: ServiceApiSiswa by lazy {
+        retrofit.create(ServiceApiSiswa::class.java)
 
 }

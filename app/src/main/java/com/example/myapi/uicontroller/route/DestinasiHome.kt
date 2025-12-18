f28377dd66058766a6a375d8822874117d43b569
icad08 <irsyadrm08@gmail.com>
2025-12-18 19:10:59 +0700

    add DestinasiHome.kt

---
+++ app/src/main/java/com/example/myapi/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.myapi.uicontroller.route
+
+import com.example.myapi.R
+
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}

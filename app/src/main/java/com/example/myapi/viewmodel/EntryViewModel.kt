b91b76c13d1bda45efa782e0d918a52a6fa54301
icad08 <irsyadrm08@gmail.com>
2025-12-18 19:11:26 +0700

    add entry view model

---
+++ app/src/main/java/com/example/myapi/viewmodel/EntryViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.myapi.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.myapi.modeldata.DetailSiswa
+import com.example.myapi.modeldata.UIStateSiswa
+import com.example.myapi.modeldata.toDataSiswa
+import com.example.myapi.repositori.RepositoryDataSiswa
+import retrofit2.Response
+
+class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
+    ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ):
+            Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput
+                (detailSiswa))
+    }
+
+    suspend fun addSiswa() {
+        if (validasiInput()) {
+            val sip:Response<Void> =repositoryDataSiswa.postDataSiswa(uiStateSiswa
+                .detailSiswa.toDataSiswa())
+            if (sip.isSuccessful){
+                println("Sukses Tambah Data : ${sip.message()}")
+            }else{
+                println("Gagal tambah data : ${sip.errorBody()}")
+            }
+        }
+    }
+}

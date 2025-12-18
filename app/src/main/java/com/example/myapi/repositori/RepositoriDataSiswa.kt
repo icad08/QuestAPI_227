8954213c827b49e3b0e25644ac6b053e33afb63e
icad08 <irsyadrm08@gmail.com>
2025-12-18 11:58:29 +0700

    add RepositoriDataSiswa.kt

--- app/src/main/java/com/example/myapi/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/myapi/repositori/RepositoriDataSiswa.kt
@@ -1,2 +1,17 @@
 package com.example.myapi.repositori
 
+import com.example.myapi.apiservice.ServiceApiSiswa
+import com.example.myapi.modeldata.DataSiswa
+import retrofit2.Response
+
+interface RepositoriDataSiswa{
+    suspend fun getDataSiswa(): List<DataSiswa>
+    suspend fun postDatasiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+}
+
+class JaringanRepositoryDataSiswa(
+    private val siswaApi: ServiceApiSiswa
+) : RepositoriDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDatasiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    }

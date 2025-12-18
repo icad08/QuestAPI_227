e96c6ada605e6bbf2aa03b5caeacd874541dcccb
icad08 <irsyadrm08@gmail.com>
2025-12-18 09:31:29 +0700

    add interface ContainerApp.kt

--- app/src/main/java/com/example/myapi/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myapi/repositori/ContainerApp.kt
@@ -1,2 +1,14 @@
 package com.example.myapi.repositori
 
+interface ContainerApp{
+    val repositoryDataSiswa : repositoryDataSiswa
+}
+
+class DefaultContainerApp : ContainerApp{
+    private val beseurl = " "
+    val logging HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+
+    val klien = okHttpClient.Builder()
+        .add
+
+}

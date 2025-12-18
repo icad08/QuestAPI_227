cad006b6095e3f64899974577169a9c667203501
icad08 <irsyadrm08@gmail.com>
2025-12-18 09:13:37 +0700

    add fun data siswa

--- app/src/main/java/com/example/myapi/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/myapi/modeldata/DataSiswa.kt
@@ -7,7 +7,7 @@ data class DataSiswa(
     val id: Int,
     val nama: String,
     val alamat: String,
-    val telepon: String
+    val telpon: String
 )
 
 data class UIStateSiswa(
@@ -19,11 +19,26 @@ data class DetailSiswa(
     val id: Int = 0,
     val nama: String = "",
     val alamat: String = "",
-    val telepon: String = "",
+    val telpon: String = "",
 )
 
 fun DetailSiswa.toDataSiswa(): DataSiswa = DataSiswa(
-    detailSiswa = this.toDetailSiswa(),
-    isEntryValid = isEntryValid()
+   id = id,
+   nama = nama,
+   alamat = alamat,
+   telpon = telpon,
 )
+
+fun DataSiswa.toUIStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+
+fun DataSiswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon,
+)
+
 

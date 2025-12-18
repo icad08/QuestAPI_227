98ba768de5a5a7590f4e9dac0734f21482f03e43
icad08 <irsyadrm08@gmail.com>
2025-12-18 19:15:23 +0700

    add properti pada fun homescreen

--- app/src/main/java/com/example/myapi/view/HomeScreen.kt
+++ app/src/main/java/com/example/myapi/view/HomeScreen.kt
@@ -1,4 +1,58 @@
 package com.example.myapi.view
 
-class HomeScreen {
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myapi.uicontroller.route.DestinasiHome
+import com.example.myapi.viewmodel.HomeViewModel
+import com.example.myapi.viewmodel.provider.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Int) -> Unit = {},
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiHome.titleRes),
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = ""
+                )
+            }
+        },
+    ) { innerPadding ->
+        HomeStatus(
+            statusUiSiswa = viewModel.listSiswa,
+            retryAction = { viewModel.loadSiswa() },
+            modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick
+        )
+    }
 }

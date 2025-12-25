02300a3f30ac542cb0c24c0da2c15c86f6d86e93
icad08 <irsyadrm08@gmail.com>
2025-12-25 17:22:13 +0700

    Implement CRUD methods in RepositoriDataSiswa

--- app/src/main/java/com/example/myapi/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/myapi/repositori/RepositoriDataSiswa.kt
@@ -7,12 +7,21 @@ import retrofit2.Response
 interface RepositoriDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDatasiswa(dataSiswa: DataSiswa): Response<Void>
+
+    // Tambahan Interface
+    suspend fun getSatuSiswa(id: Int): DataSiswa
+    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): Response<Void>
+    suspend fun hapusSatuSiswa(id: Int): Response<Void>
 }
 
 class JaringanRepositoryDataSiswa(
     private val siswaApi: ServiceApiSiswa
 ) : RepositoriDataSiswa {
-
-    override suspend fun getDataSiswa(): List<DataSiswa> = siswaApi.getSiswa()
+    override suspend fun getDataSiswa() = siswaApi.getSiswa()
     override suspend fun postDatasiswa(dataSiswa: DataSiswa) = siswaApi.postSiswa(dataSiswa)
+
+    // Tambahan Implementasi
+    override suspend fun getSatuSiswa(id: Int) = siswaApi.getSatuSiswa(id)
+    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa) = siswaApi.editSatuSiswa(id, dataSiswa)
+    override suspend fun hapusSatuSiswa(id: Int) = siswaApi.hapusSatuSiswa(id)
 }

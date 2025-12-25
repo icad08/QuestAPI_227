43a9bd6a7ab86b654b352e2d9f1fe4bb2fffc9fe
icad08 <irsyadrm08@gmail.com>
2025-12-25 17:23:44 +0700

    update destinasi detail

--- app/src/main/java/com/example/myapi/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/myapi/uicontroller/route/DestinasiDetail.kt
@@ -3,8 +3,8 @@ package com.example.myapi.uicontroller.route
 import com.example.myapi.R
 
 object DestinasiDetail : DestinasiNavigasi {
-    override val route = "item_details"
+    override val route = "detail_siswa"
     override val titleRes = R.string.detail_siswa
-    const val idSiswa = "idSiswa"
+    const val idSiswa = "itemId"
     val routeWithArgs = "$route/{$idSiswa}"
 }

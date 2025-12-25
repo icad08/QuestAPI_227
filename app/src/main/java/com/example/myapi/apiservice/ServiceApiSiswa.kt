72c51ee2cca9862aee823582f448cd5b09f89803
icad08 <irsyadrm08@gmail.com>
2025-12-25 17:21:19 +0700

    Add get, update, and delete endpoints to ServiceApiSiswa

--- app/src/main/java/com/example/myapi/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/myapi/apiservice/ServiceApiSiswa.kt
@@ -2,18 +2,23 @@ package com.example.myapi.apiservice
 
 import com.example.myapi.modeldata.DataSiswa
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.Headers
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface ServiceApiSiswa {
     @GET("bacasiswa.php")
     suspend fun getSiswa(): List<DataSiswa>
 
     @POST("insertsiswa.php")
-    @Headers("Content-Type: application/json")
     suspend fun postSiswa(@Body dataSiswa: DataSiswa): Response<Void>
 
-    companion object
+    // --- TAMBAHAN KODE DI BAWAH INI ---
+
+    @GET("bacasiswa.php/{id}")
+    suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
+
+    @PUT("editsiswa.php/{id}")
+    suspend fun editSatuSiswa(@Query("id") id: Int, @Body dataSiswa: DataSiswa): Response<Void>
+
+    @DELETE("deletesiswa.php/{id}")
+    suspend fun hapusSatuSiswa(@Query("id") id: Int): Response<Void>
 }

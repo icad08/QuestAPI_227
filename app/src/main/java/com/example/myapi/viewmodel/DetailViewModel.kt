29a8d2e90fa7d222b5dbe5fe3fd0b24419a788ed
icad08 <irsyadrm08@gmail.com>
2025-12-25 17:33:11 +0700

    deklarasi variable

--- app/src/main/java/com/example/myapi/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/myapi/viewmodel/DetailViewModel.kt
@@ -1,11 +1,18 @@
 package com.example.myapi.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.myapi.modeldata.DataSiswa
+import com.example.myapi.repositori.RepositoriDataSiswa
 
 sealed interface DetailsUiState {
     data class Success(val detailSiswa: DataSiswa) : DetailsUiState
     object Error : DetailsUiState
     object Loading : DetailsUiState
-
-class DetailViewModel {
 }
+
+class DetailViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoriDataSiswa: RepositoriDataSiswa
+) : ViewModel()
+)

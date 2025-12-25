abaf2258d1072af34be7aac2181d6b72d5ae6f00
icad08 <irsyadrm08@gmail.com>
2025-12-25 17:39:46 +0700

    Register DetailViewModel and EditViewModel in Factory

--- app/src/main/java/com/example/myapi/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myapi/viewmodel/provider/PenyediaViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.myapi.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.myapi.repositori.AplikasiDataSiswa
+import com.example.myapi.viewmodel.DetailViewModel
+import com.example.myapi.viewmodel.EditViewModel
 import com.example.myapi.viewmodel.EntryViewModel
 import com.example.myapi.viewmodel.HomeViewModel
 
@@ -14,11 +17,22 @@ fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
+        initializer { EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
+
+        // TAMBAHAN
         initializer {
-            HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
+            DetailViewModel(
+                createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
         }
         initializer {
-            EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
+            EditViewModel(
+                createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
         }
     }
 }
+

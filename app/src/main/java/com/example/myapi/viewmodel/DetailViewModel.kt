0d7d419ee4cd057fc7c2a5901f418dd6751372f6
icad08 <irsyadrm08@gmail.com>
2025-12-25 17:34:15 +0700

    add function getSatuSiswa

--- app/src/main/java/com/example/myapi/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/myapi/viewmodel/DetailViewModel.kt
@@ -1,9 +1,14 @@
 package com.example.myapi.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.myapi.modeldata.DataSiswa
 import com.example.myapi.repositori.RepositoriDataSiswa
+import com.example.myapi.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 sealed interface DetailsUiState {
     data class Success(val detailSiswa: DataSiswa) : DetailsUiState
@@ -14,5 +19,25 @@ sealed interface DetailsUiState {
 class DetailViewModel (
     savedStateHandle: SavedStateHandle,
     private val repositoriDataSiswa: RepositoriDataSiswa
-) : ViewModel()
+) : ViewModel(){
+
+    private val siswaId: Int = checkNotNull(savedStateHandle[DestinasiDetail.idSiswa])
+    var detailUiState: DetailsUiState by mutableStateOf(DetailsUiState.Loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
+    fun getSatuSiswa() {
+        viewModelScope.launch {
+            detailUiState = DetailsUiState.Loading
+            detailUiState = try {
+                DetailsUiState.Success(repositoriDataSiswa.getSatuSiswa(siswaId))
+            } catch (e: IOException) {
+                DetailsUiState.Error
+            } catch (e: Exception) {
+                DetailsUiState.Error
+            }
+        }
+    }
 )
